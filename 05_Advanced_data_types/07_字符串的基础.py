2a0b70259f89bf1f29fa11a30e195b2b2cfd811f
ic3evi1 <270490285@qq.com>
2019-11-05 15:54:14 +0800

    字符串基础

---
+++ 05_Advanced_data_types/07_字符串的基础.py
@@ -0,0 +1,19 @@
+# 定义一个字符串
+string = "hellO pythOn"
+# 字符串取值
+print(string[2])
+print(len(string))
+print("-" * 40)
+# 字符串遍历
+for str1 in string:
+    print(str1)
+print("-" * 40)
+# 字符串大小写转换
+print(string.title())
+print(string.upper())
+print(string.lower())
+print(string.capitalize())
+print(string.swapcase())
+print("-" * 40)
+
+
