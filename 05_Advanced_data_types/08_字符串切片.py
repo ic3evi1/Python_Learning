065d7cad3e72c0ba9081b63609dd3bbb22a076e9
ic3evi1 <270490285@qq.com>
2019-11-06 14:25:31 +0800

    字符串切片

---
+++ 05_Advanced_data_types/08_字符串切片.py
@@ -0,0 +1,18 @@
+num_string = "0123456789"
+
+#输出2-8
+
+print(num_string[2:9])
+
+#输出偶数
+
+print(num_string[::2])
+
+#输出奇数
+
+print(num_string[1::2])
+
+#逆序输出
+
+print(num_string[-1:0:-1])
+print(num_string[::-1])
